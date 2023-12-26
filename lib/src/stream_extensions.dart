249cbb3473011f8169d4bea86d8c77eb8acff5a4
eaglesakura <eagle.sakura@gmail.com>
2023-12-26 17:17:52 +0900

    fix: dart2jsのコンパイラがクラッシュする問題を修正

--- lib/src/stream_extensions.dart
+++ lib/src/stream_extensions.dart
@@ -24,74 +24,13 @@ extension StreamWithContext<T> on Stream<T> {
     bool withDebugLog = kDebugMode,
   }) {
     final stackTrace = StackTrace.current;
-    final key = this;
     late final PublishSubject<T> subject;
     bool launched = false;
 
-    void _log(String message) {
+    void log(String message) {
       if (withDebugLog) {
         debugPrint(
             '${DateTime.now().toIso8601String()} [Stream<$T>.withContext] $message');
-      }
-    }
-
-    /// データの受領と再送を行う.
-    /// Stream内部で例外が発生する場合があるため、データはすべて受領する.
-    Future proxy() async {
-      try {
-        await for (final v in this) {
-          _notify.add(key);
-          if (subject.isClosed) {
-            if (receiveAllValues) {
-              continue;
-            }
-            return;
-          } else {
-            subject.add(v);
-          }
-        }
-        // ignore: avoid_catches_without_on_clauses
-      } catch (e, stackTrace) {
-        if (!subject.isClosed) {
-          subject.addError(e, stackTrace);
-        } else {
-          _log(
-              '${context.toString()} is canceled, drop error: $e\n$stackTrace');
-        }
-      } finally {
-        _log('proxy close');
-        _notify.add(key);
-        if (!subject.isClosed) {
-          unawaited(subject.close());
-        }
-      }
-    }
-
-    /// キャンセル対応を行う.
-    /// キャンセルされた場合、[subject]にエラーを流して、[subject]をcloseする.
-    Future cancellation() async {
-      final stream = CombineLatestStream.combine2(
-        context.isCanceledStream,
-        _notify.where((event) => identical(event, key)),
-        (a, b) => a,
-      );
-      try {
-        await for (final isCanceled in stream) {
-          if (subject.isClosed) {
-            return;
-          } else if (!isCanceled) {
-            continue;
-          } else if (!subject.isClosed) {
-            subject.addError(
-                CancellationException(
-                    '[Stream<$T>.withContext] ${context.toString()} is canceled'),
-                stackTrace);
-            unawaited(subject.close());
-          }
-          return;
-        }
-      } finally {
-        _log('cancellation close');
       }
     }
 
@@ -100,10 +39,91 @@ extension StreamWithContext<T> on Stream<T> {
         return;
       }
       launched = true;
-      unawaited(proxy());
-      unawaited(cancellation());
+      unawaited(_proxy(
+        context,
+        receiveAllValues,
+        subject,
+        log,
+      ));
+      unawaited(_cancellation(
+        context,
+        subject,
+        stackTrace,
+        log,
+      ));
     });
 
     return subject.stream;
+  }
+
+  /// キャンセル対応を行う.
+  /// キャンセルされた場合、[subject]にエラーを流して、[subject]をcloseする.
+  Future _cancellation(
+    FutureContext context,
+    Subject<T> subject,
+    StackTrace stackTrace,
+    void Function(String message) log,
+  ) async {
+    final key = this;
+    final stream = CombineLatestStream.combine2(
+      context.isCanceledStream,
+      _notify.where((event) => identical(event, key)),
+      (a, b) => a,
+    );
+    try {
+      await for (final isCanceled in stream) {
+        if (subject.isClosed) {
+          return;
+        } else if (!isCanceled) {
+          continue;
+        } else if (!subject.isClosed) {
+          subject.addError(
+              CancellationException(
+                  '[Stream<$T>.withContext] ${context.toString()} is canceled'),
+              stackTrace);
+          unawaited(subject.close());
+        }
+        return;
+      }
+    } finally {
+      log('cancellation close');
+    }
+  }
+
+  /// データの受領と再送を行う.
+  /// Stream内部で例外が発生する場合があるため、データはすべて受領する.
+  Future _proxy(
+    FutureContext context,
+    bool receiveAllValues,
+    Subject<T> subject,
+    void Function(String message) log,
+  ) async {
+    final key = this;
+    try {
+      await for (final v in this) {
+        _notify.add(key);
+        if (subject.isClosed) {
+          if (receiveAllValues) {
+            continue;
+          }
+          return;
+        } else {
+          subject.add(v);
+        }
+      }
+      // ignore: avoid_catches_without_on_clauses
+    } catch (e, stackTrace) {
+      if (!subject.isClosed) {
+        subject.addError(e, stackTrace);
+      } else {
+        log('${context.toString()} is canceled, drop error: $e\n$stackTrace');
+      }
+    } finally {
+      log('proxy close');
+      _notify.add(key);
+      if (!subject.isClosed) {
+        unawaited(subject.close());
+      }
+    }
   }
 }
